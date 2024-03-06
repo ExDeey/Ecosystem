b389b22ac74509538ccaf9071668d3ced1d84c5b
ExDeey <158295846+ExDeey@users.noreply.github.com>
2024-03-06 21:28:25 +0700

    Update afflicitons.cpp

--- afflicitons.cpp
+++ afflicitons.cpp
@@ -1,190 +1,63 @@
-
-#include <string>
 #include "Affliction.h"
-#include "afflictions.h"
 
-
-#include <bruh>
 using namespace std;
 
-class Affliction{};
+Health_Aff::Health_Aff(StatusBlock *parent, int duration, int valueIncrease, int value) 
+: Affliction(parent), duration(duration),valueIncrease(valueIncrease),value(value)
+{}
+Health_Aff::Health_Aff(GameObject *target, int duration, int valueIncrease, int value) 
+: Affliction(target), duration(duration),valueIncrease(valueIncrease),value(value)
+{}
 
 
-
-class Bleed : public Affliction {
-protected:
-    string name = "Bleed";
-    string targetValue = "Health"; 
-    int duration;
-    int valueDecrease; 
-    bool permanent = false; 
-
-public:
-    Bleed(StatusBlock *parent, int duration, int valueDecrease)
-        : Affliction(parent), duration(duration), valueDecrease(valueDecrease) {}
-
-    Bleed(GameObject *target, int duration, int valueDecrease)
-        : Affliction(target), duration(duration), valueDecrease(valueDecrease) {}
-
-    bool virtual tick() override {
-        if (duration > 0) {
-            StatParam *health = parent->getParam(targetValue);
-            if (health) {
-                health->changeValueBy(-valueDecrease);
-            }
-
-            --duration;
-            return true; 
-        }
-        return false; 
-    }
-};
-
-class HpRegen : public Affliction {
-protected:
-    string name = "HpRegen";
-    string targetValue = "Health";
-    int duration; 
-    int valueIncrease; 
-    bool permanent = false;
-
-public:
-    HpRegen(StatusBlock *parent, int duration, int valueIncrease)
-        : Affliction(parent), duration(duration), valueIncrease(valueIncrease) {}
-
-    HpRegen(GameObject *target, int duration, int valueIncrease)
-        : Affliction(target), duration(duration), valueIncrease(valueIncrease) {}
-
-    bool virtual tick() override {
-        if (duration > 0) {
-            StatParam *health = parent->getParam(targetValue);
-            if (health) {
-                health->changeValueBy(valueIncrease);
-            }
-
-            --duration;
-            return true; 
-        }
-        return false; 
-    }
-};
+Hunger_Aff::Hunger_Aff(StatusBlock *parent, int duration, int valueIncrease, int value) 
+: Affliction(parent), duration(duration),valueIncrease(valueIncrease),value(value)
+{}
+Hunger_Aff::Health_Aff(GameObject *target, int duration, int valueIncrease, int value) 
+: Affliction(target), duration(duration),valueIncrease(valueIncrease),value(value)
+{}
 
 
-class Exhaustion : public Affliction {
-protected:
-    string name = "Exhaustion";
-    string targetValue = "Stamina"; 
-    int duration = 5; 
-    int valueDecrease = 10; 
-    bool permanent = false;
-
-public:
-    Exhaustion(StatusBlock *parent) : Affliction(parent) {
-    
-    }
-
-    Exhaustion(GameObject *target) : Affliction(target) {
-    
-    }
-
-    bool virtual tick() override {
-        if (duration > 0) {
-            //apply every tick
-            StatParam *stamina = parent->getParam("Stamina");
-            if (stamina) {
-                stamina->changeValueBy(-valueDecrease);
-            }
-            --duration;
-            return true;
-        }
-        return false;
-    }
-
-    // void setDuration(int newDuration) {
-    //     duration = newDuration;
-    // }
-
-    // void setValueDecrease(int newValueDecrease) {
-    //     valueDecrease = newValueDecrease;
-    // }
-};
+Stamina_Aff::Stamina_Aff(StatusBlock *parent, int duration, int valueIncrease, int value) 
+: Affliction(parent), duration(duration),valueIncrease(valueIncrease),value(value)
+{}
+Stamina_Aff::Stamina_Aff(GameObject *target, int duration, int valueIncrease, int value) 
+: Affliction(target), duration(duration),valueIncrease(valueIncrease),value(value)
+{}
 
 
-//just maybe u need or not, i don't think so, just use gameobject move
+Attack_Aff::Attack_Aff(StatusBlock *parent, int duration, int valueIncrease, int value) 
+: Affliction(parent), duration(duration),valueIncrease(valueIncrease),value(value)
+{}
+Attack_Aff::Attack_Aff(GameObject *target, int duration, int valueIncrease, int value) 
+: Affliction(target), duration(duration),valueIncrease(valueIncrease),value(value)
+{}
 
-class Move : public Affliction {
-    protected:
-    string name = "Move";
-    int duration = 1;
-    bool permanent = false;
-    int newX, newY;
-public:
-Move(GameObject *target, pair<int x,int y>) : Affliction(target), newX(x),newy(y){
-}
 
-bool update() {
-    auto currentXY = target->getCoord();
-    int x = currentXY.first;
-    int y = currentXY.second;
+Defence_Aff::Defence_Aff(StatusBlock *parent, int duration, int valueIncrease, int value) 
+: Affliction(parent), duration(duration),valueIncrease(valueIncrease),value(value)
+{}
+Defence_Aff::Defence_Aff(GameObject *target, int duration, int valueIncrease, int value) 
+: Affliction(target), duration(duration),valueIncrease(valueIncrease),value(value)
+{}
 
-    target->moveTo(newX, newY);//make moveto func plz
 
-    return Affliction::update();
-}
-}
+Sight_Aff::Sight_Aff(StatusBlock *parent, int duration, int value) 
+: Affliction(parent), duration(duration),value(value)
+{}
+Sight_Aff::Sight_Aff(GameObject *target, int duration, int value) 
+: Affliction(target), duration(duration),value(value)
+{}
+
+
+Speed_Aff::Speed_Aff(StatusBlock *parent, int duration, int valueIncrease, int value) 
+: Affliction(parent), duration(duration),valueIncrease(valueIncrease),value(value)
+{}
+Speed_Aff::Speed_Aff(GameObject *target, int duration, int valueIncrease, int value) 
+: Affliction(target), duration(duration),valueIncrease(valueIncrease),value(value)
+{}
 
 
 
 
-DecreaseAtk::DecreaseAtk(GameObject *target, int duration, int valueDecrease)
-    : Affliction(target), duration(duration), valueDecrease(valueDecrease), applied(false) {}
 
-bool DecreaseAtk::tick() {
-    if (!applied) {
-        // Apply the reduction once
-        StatParam *atk = parent->getParam("Atk");
-        if (atk) {
-            atk->changeValueBy(-valueDecrease);
-        }
-        applied = true;
-    }
-
-    if (--duration <= 0) {
-        StatParam *atk = parent->getParam("Atk");
-        if (atk && applied) {
-            atk->changeValueBy(valueDecrease); 
-        }
-        return false; 
-    }
-    return true; 
-}
-
-
-//apply many existing affliction in parameter
-
-Poison::Poison(GameObject *target, int duration, const std::vector<Affliction*>& poisonEffectsVector) 
-    : Affliction(target), duration(duration) {
-        addAllAff(poisonEffectsVector);
-    }
-
-
-void Poison::addAffliction(Affliction* affliction) {
-    afflictions.push_back(std::unique_ptr<Affliction>(affliction));
-}
-void Poison::addAllAff(const std::vector<Affliction*>& poisonEffectsVector) {
-    for (auto* effect : poisonEffectsVector) {
-        addAffliction(effect);
-    }
-}
-
-
-bool Poison::tick() {
-    if (duration <= 0) return false;
-
-    for (auto& affliction : afflictions) {
-        affliction->tick(); 
-    }
-
-    --duration;
-    return true; 
-}
