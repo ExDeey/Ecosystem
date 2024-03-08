f21ec65c6a7e68087c55eea7521bbeb9fd8ea57b
ExDeey <158295846+ExDeey@users.noreply.github.com>
2024-03-08 23:05:20 +0700

    Update afflicitons.cpp

--- afflicitons.cpp
+++ afflicitons.cpp
@@ -2,51 +2,51 @@
 
 using namespace std;
 
-Health_Aff::Health_Aff(StatusBlock *parent, int duration, int valueIncrease, int value) 
-: Affliction(parent), duration(duration),valueIncrease(valueIncrease),value(value)
+Health_Aff::Health_Aff(StatusBlock *parent, int duration, int valueIncrease, int value, bool permanent) 
+: Affliction(parent), duration(duration),valueIncrease(valueIncrease),value(value), permanent(permanent)
 {}
-Health_Aff::Health_Aff(GameObject *target, int duration, int valueIncrease, int value) 
-: Affliction(target), duration(duration),valueIncrease(valueIncrease),value(value)
-{}
-
-
-Hunger_Aff::Hunger_Aff(StatusBlock *parent, int duration, int valueIncrease, int value) 
-: Affliction(parent), duration(duration),valueIncrease(valueIncrease),value(value)
-{}
-Hunger_Aff::Health_Aff(GameObject *target, int duration, int valueIncrease, int value) 
-: Affliction(target), duration(duration),valueIncrease(valueIncrease),value(value)
+Health_Aff::Health_Aff(GameObject *target, int duration, int valueIncrease, int value, bool permanent) 
+: Affliction(target), duration(duration),valueIncrease(valueIncrease),value(value), permanent(permanent)
 {}
 
 
-Stamina_Aff::Stamina_Aff(StatusBlock *parent, int duration, int valueIncrease, int value) 
-: Affliction(parent), duration(duration),valueIncrease(valueIncrease),value(value)
+Hunger_Aff::Hunger_Aff(StatusBlock *parent, int duration, int valueIncrease, int value, bool permanent) 
+: Affliction(parent), duration(duration),valueIncrease(valueIncrease),value(value), permanent(permanent)
 {}
-Stamina_Aff::Stamina_Aff(GameObject *target, int duration, int valueIncrease, int value) 
-: Affliction(target), duration(duration),valueIncrease(valueIncrease),value(value)
-{}
-
-
-Attack_Aff::Attack_Aff(StatusBlock *parent, int duration, int valueIncrease, int value) 
-: Affliction(parent), duration(duration),valueIncrease(valueIncrease),value(value)
-{}
-Attack_Aff::Attack_Aff(GameObject *target, int duration, int valueIncrease, int value) 
-: Affliction(target), duration(duration),valueIncrease(valueIncrease),value(value)
+Hunger_Aff::Health_Aff(GameObject *target, int duration, int valueIncrease, int value, bool permanent) 
+: Affliction(target), duration(duration),valueIncrease(valueIncrease),value(value), permanent(permanent)
 {}
 
 
-Defence_Aff::Defence_Aff(StatusBlock *parent, int duration, int valueIncrease, int value) 
-: Affliction(parent), duration(duration),valueIncrease(valueIncrease),value(value)
+Stamina_Aff::Stamina_Aff(StatusBlock *parent, int duration, int valueIncrease, int value, bool permanent) 
+: Affliction(parent), duration(duration),valueIncrease(valueIncrease),value(value), permanent(permanent)
 {}
-Defence_Aff::Defence_Aff(GameObject *target, int duration, int valueIncrease, int value) 
-: Affliction(target), duration(duration),valueIncrease(valueIncrease),value(value)
+Stamina_Aff::Stamina_Aff(GameObject *target, int duration, int valueIncrease, int value, bool permanent) 
+: Affliction(target), duration(duration),valueIncrease(valueIncrease),value(value), permanent(permanent)
 {}
 
 
-Sight_Aff::Sight_Aff(StatusBlock *parent, int duration, int value) 
-: Affliction(parent), duration(duration),value(value)
+Attack_Aff::Attack_Aff(StatusBlock *parent, int duration, int valueIncrease, int value, bool permanent) 
+: Affliction(parent), duration(duration),valueIncrease(valueIncrease),value(value), permanent(permanent)
 {}
-Sight_Aff::Sight_Aff(GameObject *target, int duration, int value) 
-: Affliction(target), duration(duration),value(value)
+Attack_Aff::Attack_Aff(GameObject *target, int duration, int valueIncrease, int value, bool permanent) 
+: Affliction(target), duration(duration),valueIncrease(valueIncrease),value(value), permanent(permanent)
+{}
+
+
+Defence_Aff::Defence_Aff(StatusBlock *parent, int duration, int valueIncrease, int value, bool permanent) 
+: Affliction(parent), duration(duration),valueIncrease(valueIncrease),value(value), permanent(permanent)
+{}
+Defence_Aff::Defence_Aff(GameObject *target, int duration, int valueIncrease, int value, bool permanent) 
+: Affliction(target), duration(duration),valueIncrease(valueIncrease),value(value), permanent(permanent)
+{}
+
+
+Sight_Aff::Sight_Aff(StatusBlock *parent, int duration, int value, bool permanent) 
+: Affliction(parent), duration(duration),value(value), permanent(permanent)
+{}
+Sight_Aff::Sight_Aff(GameObject *target, int duration, int value, bool permanent) 
+: Affliction(target), duration(duration),value(value), permanent(permanent)
 {}
 
 
@@ -56,7 +56,6 @@ Speed_Aff::Speed_Aff(StatusBlock *parent, int duration, int valueIncrease, int v
 Speed_Aff::Speed_Aff(GameObject *target, int duration, int valueIncrease, int value) 
 : Affliction(target), duration(duration),valueIncrease(valueIncrease),value(value)
 {}
-
 
 
 
