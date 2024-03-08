6edb49051c647e767796a8ec3a04d2d08ed9d3d7
ExDeey <158295846+ExDeey@users.noreply.github.com>
2024-03-08 23:06:10 +0700

    Create Living_thing.cpp

---
+++ Living_thing.cpp
@@ -0,0 +1,53 @@
+#include "living_thing.h"
+
+
+
+Animal::Animal(Layer *parent, int x, int)
+        : LivingThing(parent, x, y){
+        initializeStats();
+        initializeAfflictions();
+        initializeAbilities();        }
+
+
+
+Plant::Plant(Layer *parent, int x, int )
+        : LivingThing(parent, x, y){
+        initializeStats();
+        initializeAfflictions();
+        initializeAbilities();
+        }
+
+
+
+Lion::Lion(Layer *parent, int x, int y)
+: Animal(parent, x, y){
+    Health = 100;
+    Hunger = 50;
+    Stamina =50;
+    Atk = 40;
+    Def = 10;
+    Sight = 3;
+    Speed = 10;
+    Defence_Aff* defenceAffliction = new Defence_Aff(this, 10, 5, -20, false);
+    addAffliction(new defenceAffliction());
+    // this->addAffliction(new Aff_Attack());
+
+    //check plz idk bruh
+    this->addAffliction(new Aff_Speed());
+    this->addAbility(new sleep());
+    this->addAbility(new die());
+    }
+
+
+
+
+// void Lion::initializeAfflictions() {
+//         //Affliction* aff_effect = new HungerAffliction();
+//         this->addAffliction(new Aff_Attack());
+//         this->addAffliction(new Aff_Speed());
+//     }
+
+// void Lion::initializeAbilities() {
+//         this->addAbility(new sleep());
+//         this->addAbility(new die());
+//     }
