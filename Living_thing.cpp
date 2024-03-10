e8b5d2b5a668cf252c3f4a31f29c62cb7b13c665
ExDeey <158295846+ExDeey@users.noreply.github.com>
2024-03-10 18:28:19 +0700

    Update Living_thing.cpp

--- Living_thing.cpp
+++ Living_thing.cpp
@@ -17,25 +17,205 @@ Plant::Plant(Layer *parent, int x, int )
 
 Lion::Lion(Layer *parent, int x, int y)
 : Animal(parent, x, y){
-    Health = 100;
-    Hunger = 50;
-    Stamina =50;
-    Atk = 40;
-    Def = 10;
-    Sight = 3;
-    Speed = 10;
-    Defence_Aff* defenceAffliction = new Defence_Aff(this, 10, 5, -20, false);
-    addAffliction(new defenceAffliction());
-    // this->addAffliction(new Aff_Attack());
+    Health = 160;
+    Sight = 5;
+    Speed = 5;
 
-    //check plz idk bruh
-    this->addAffliction(new Aff_Speed());
-    this->addAbility(new sleep());
-    this->addAbility(new die());
+    new PlayTurn();
+    new Eat();
+    new Rest();
+    new Attack();
+    new Walk();
+    new Mate();
+    new Attack_Aff(this, 0, 0, 50, true);
+    new Stamina_Aff(this, 0, 0, 50, true);
+    
+    }
+
+Bear::Bear(Layer *parent, int x, int y)
+: Animal(parent, x, y){
+    Health = 120;
+    Sight = 5;
+    Speed = 5;
+
+    new PlayTurn();
+    new Eat();
+    new Rest();
+    new Attack();
+    new Walk();
+    new Mate();
+    new Attack_Aff(this, 0, 0, 50, true);
+    new Stamina_Aff(this, 0, 0, 50, true);
+    
+    }
+
+Cheetah::Cheetah(Layer *parent, int x, int y)
+: Animal(parent, x, y){
+    Health = 80;
+    Sight = 5;
+    Speed = 7;
+
+    new PlayTurn();
+    new Eat();
+    new Rest();
+    new Attack();
+    new Walk();
+    new Mate();
+    new Attack_Aff(this, 0, 0, 30, true);
+    new Stamina_Aff(this, 0, 0, 70, true);
+    
+    }
+
+Giant_Snake::Giant_Snake(Layer *parent, int x, int y)
+: Animal(parent, x, y){
+    Health = 60;
+    Sight = 5;
+    Speed = 3;
+
+    new PlayTurn();
+    new Eat();
+    new Rest();
+    new Walk();
+    new Mate();
+    new Poison_Attack();
+    new Attack_Aff(this, 0, 0, 20, true);
+    new Stamina_Aff(this, 0, 0, 20, true);
+    
+    }
+
+Rabbit::Rabbit(Layer *parent, int x, int y)
+: Animal(parent, x, y){
+    Health = 30;
+    Sight = 2;
+    Speed = 2;
+
+    new PlayTurn();
+    new Eat();
+    new Rest();
+    new Attack();
+    new Walk();
+    new Mate();
+
+    new Attack_Aff(this, 0, 0, 10, true);
+    new Stamina_Aff(this, 0, 0, 10, true);
+    
+    }
+
+Goat::Goat(Layer *parent, int x, int y)
+: Animal(parent, x, y){
+    Health = 40;
+
+    Sight = 2;
+    Speed = 2;
+
+    new PlayTurn();
+    new Eat();
+    new Rest();
+    new Attack();
+    new Walk();
+    new Mate();
+
+    new Attack_Aff(this, 0, 0, 10, true);
+    new Stamina_Aff(this, 0, 0, 10, true);
+    
+    }
+
+Deer::Deer(Layer *parent, int x, int y)
+: Animal(parent, x, y){
+    Health = 40;
+    Sight = 2;
+    Speed = 2;
+
+    new PlayTurn();
+    new Eat();
+    new Rest();
+    new Attack();
+    new Walk();
+    new Mate();
+
+    new Attack_Aff(this, 0, 0, 10, true);
+    new Stamina_Aff(this, 0, 0, 10, true);
+    
+    }
+
+Chicken::Chicken(Layer *parent, int x, int y)
+: Animal(parent, x, y){
+    Health = 20;
+    Sight = 2;
+    Speed = 2;
+
+    new PlayTurn();
+    new Eat();
+    new Rest();
+    new Attack();
+    new Walk();
+    new Mate();
+
+    new Attack_Aff(this, 0, 0, 10, true);
+    new Stamina_Aff(this, 0, 0, 10, true);
+    
+    }
+
+Poison_Frog::Poison_Frog(Layer *parent, int x, int y)
+: Animal(parent, x, y){
+    Health = 20;
+   
+    Sight = 2;
+    Speed = 2;
+
+    new PlayTurn();
+    new Eat();
+    new Rest();
+    new Attack();
+    new Walk();
+    new Mate();
+    new Poison_skin();
+    new Attack_Aff(this, 0, 0, 10, true);
+    new Stamina_Aff(this, 0, 0, 10, true);
+    
     }
 
 
+EarthWorm::EarthWorm(Layer *parent, int x, int y)
+: Animal(parent, x, y){
+    Health = 5;
+    Sight = 2;
+    Speed = 2;
 
+    new PlayTurn();
+    
+    new Walk();
+    
+    }
+
+Bush::Bush(Layer *parent, int x, int y)
+: Plant(parent, x, y){
+    Health = 5;
+
+    new PlayTurn();
+    new Spawn_fruit();
+    new Walk();
+    
+    }
+
+Apple_tree::Apple_tree(Layer *parent, int x, int y)
+: Plant(parent, x, y){
+    Health = 5;
+
+    new PlayTurn();
+    new Spawn_fruit();
+    new Walk();
+    
+    }
+
+Mushroom::Mushroom(Layer *parent, int x, int y)
+: Plant(parent, x, y){
+    Health = 5;
+
+    new PlayTurn();
+    new Walk();
+    
+    }
 
 // void Lion::initializeAfflictions() {
 //         //Affliction* aff_effect = new HungerAffliction();
