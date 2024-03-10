76a6f60b03a1b1466dc4cb5117795b2d90f12a1a
ExDeey <158295846+ExDeey@users.noreply.github.com>
2024-03-10 18:20:53 +0700

    Update living_thing.h

--- living_thing.h
+++ living_thing.h
@@ -42,7 +42,7 @@ public:
 
 
 class Plant : public GameObject {
-    
+
 protected:
     int Health = 0;int Atk = 0;int Def = 0;int Sight = 0;int Speed = 0;
     string Type = "Plant";
@@ -57,20 +57,207 @@ public:
 
 };
 
+class Corpse : public GameObject {
+
+protected:
+    int Hunger_yield;
+    string size, previous_type;
+    string Type = "Corpse";
+
+public:
+    Corpse(Layer *parent, int x, int y, string previous_type, string size);
+    
+    virtual void update() override {
+        GameObject::update(); 
+    };
+    
+
+};
+
 class Lion : public Animal {
 protected:
     int Health, Hunger, Stamina, Atk, Def, Sight, Speed;
-
+    string name = "Lion";
+    string diet_type = "Carnivore";
+    string size = "Big";
 public:
     Lion(Layer *parent, int x, int y);
-    
-
     virtual void update() override {
         GameObject::update(); 
     };
 
     
 };
+
+class Bear : public Animal {
+protected:
+    int Health, Hunger, Stamina, Atk, Def, Sight, Speed;
+    string name = "Bear";
+    string diet_type = "Carnivore";
+    string size = "Big";
+public:
+    Bear(Layer *parent, int x, int y);
+    virtual void update() override {
+        GameObject::update(); 
+    };
+};
+
+class Cheetah : public Animal {
+protected:
+    int Health, Hunger, Stamina, Atk, Def, Sight, Speed;
+    string name = "Cheetah";
+    string diet_type = "Carnivore";
+    string size = "Big";
+public:
+    Cheetah(Layer *parent, int x, int y);
+    virtual void update() override {
+        GameObject::update(); 
+    };
+};
+
+class Giant_Snake : public Animal {
+protected:
+    int Health, Hunger, Stamina, Atk, Def, Sight, Speed;
+    string name = "Giant Snake";
+    string diet_type = "Carnivore";
+    string size = "Big";
+public:
+    Giant_Snake(Layer *parent, int x, int y);
+    virtual void update() override {
+        GameObject::update(); 
+    };
+
+    
+};
+
+class Rabbit : public Animal {
+protected:
+    int Health, Hunger, Stamina, Atk, Def, Sight, Speed;
+    string name = "Rabbit";
+    string diet_type = "Herbivore";
+    string size = "Medium";
+public:
+    Rabbit(Layer *parent, int x, int y);
+    virtual void update() override {
+        GameObject::update(); 
+    };
+};
+
+
+class Goat : public Animal {
+protected:
+    int Health, Hunger, Stamina, Atk, Def, Sight, Speed;
+    string name = "Goat";
+    string diet_type = "Herbivore";
+    string size = "Medium";
+public:
+    Rabbit(Layer *parent, int x, int y);
+    virtual void update() override {
+        GameObject::update(); 
+    };
+};
+
+
+class Deer : public Animal {
+protected:
+    int Health, Hunger, Stamina, Atk, Def, Sight, Speed;
+    string name = "Deer";
+    string diet_type = "Herbivore";
+    string size = "Medium";
+public:
+    Deer(Layer *parent, int x, int y);
+    virtual void update() override {
+        GameObject::update(); 
+    };
+};
+
+class Chicken : public Animal {
+protected:
+    int Health, Hunger, Stamina, Atk, Def, Sight, Speed;
+    string name = "Chicken";
+    string diet_type = "Omnivore";
+    string size = "Medium";
+public:
+    Chicken(Layer *parent, int x, int y);
+    virtual void update() override {
+        GameObject::update(); 
+    };
+};
+
+class Poison_Frog : public Animal {
+protected:
+    int Health, Hunger, Stamina, Atk, Def, Sight, Speed;
+    string name = "Poison Frog";
+    string diet_type = "Omnivore";
+    string size = "Medium";
+public:
+    Poison_Frog(Layer *parent, int x, int y);
+    virtual void update() override {
+        GameObject::update(); 
+    };
+};
+
+class EarthWorm : public Animal {
+protected:
+    int Health, Hunger, Stamina, Atk, Def, Sight, Speed;
+    string name = "EarthWorm";
+    string diet_type = "Special";
+    string size = "Small";
+public:
+    EarthWorm(Layer *parent, int x, int y);
+    virtual void update() override {
+        GameObject::update(); 
+    };
+};
+
+
+class Bush : public Plant {
+protected:
+    int Health;
+    string name = "Bush";
+    
+public:
+    Bush(Layer *parent, int x, int y);
+    virtual void update() override {
+        GameObject::update(); 
+    };
+};
+
+class Apple_tree : public Plant {
+protected:
+    int Health, fruits, hunger_yield;
+    string name = "Apple tree";
+    
+public:
+    Apple_tree(Layer *parent, int x, int y);
+    virtual void update() override {
+        GameObject::update(); 
+    };
+};
+
+class Mushroom : public Plant {
+protected:
+    int Health,hunger_yield;
+    string name = "Mushroom";
+    
+public:
+    Mushroom(Layer *parent, int x, int y);
+    virtual void update() override {
+        GameObject::update(); 
+    };
+};
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
